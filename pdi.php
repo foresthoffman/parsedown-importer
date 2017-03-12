20eed682022230882e824a13b184abe25bbd8cb4
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-03-11 21:11:13 -0800

    Init repo

---
+++ pdi.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Parsedown Importer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     pdi
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pdi
+ */
